299f13e6fa02ff4fe2a3d3145008ba18564e8e32
Aravind <aravindmunna240@gmail.com>
2024-11-28 12:28:43 +0530

    String : Escaped strings and multiline strings

---
+++ src/main/kotlin/StringsOperations.kt
@@ -0,0 +1,28 @@
+
+
+fun escapedString(){
+    val s = "Hello, world!\n"
+
+    print(s)
+}
+
+fun multilineStrings(){
+
+    val text = """
+    Hello, Aravind!
+    This is an example of a "multiline string."
+    Use it to write whatever you want: text, symbols, or even code snippets.
+    
+    Kotlin is fun! 😊
+"""
+
+    print(text)
+
+    val newTestText = """
+       Hi 
+       Hello
+       This is a test function
+    """
+
+    print(newTestText)
+}
