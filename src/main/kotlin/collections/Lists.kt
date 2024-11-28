d79331b4653934cbed5257bdb9f8573ee526a427
Aravind <aravindmunna240@gmail.com>
2024-11-29 00:08:21 +0530

    Lists: Mutable lists and normal lists in kotlin

--- src/main/kotlin/collections/Lists.kt
+++ src/main/kotlin/collections/Lists.kt
@@ -3,6 +3,6 @@ package collections
 fun listLearn(){
     val myList = listOf("One", "Two", "Three")
     val readOnly: MutableList<String> = myList.toMutableList()
-    print(myList)
+    println(myList)
 
 }
