c0236b03cc859dbdad624e37f96473dbd5303476
Aravind <aravindmunna240@gmail.com>
2024-11-29 00:09:27 +0530

    Collections , Kotlin : Sets mutable and immutable and setOf Methods

--- src/main/kotlin/collections/Sets.kt
+++ src/main/kotlin/collections/Sets.kt
@@ -1,2 +1,10 @@
 package collections
 
+fun setLearn(){
+    val mySet = setOf<String>("One", "Two", "One")
+    println(mySet)
+
+    val newSetCreation : MutableSet<String> = HashSet()
+    newSetCreation.add("Three")
+    println(newSetCreation)
+}
