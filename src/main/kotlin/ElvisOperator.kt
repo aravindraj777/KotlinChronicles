56968889d6f3ff03b5c099f950eeebab74c12236
Aravind <aravindmunna240@gmail.com>
2024-11-29 11:51:47 +0530

    Assertion operator and exception hanling with try,catch block

--- src/main/kotlin/ElvisOperator.kt
+++ src/main/kotlin/ElvisOperator.kt
@@ -1,3 +1,4 @@
+import java.lang.Exception
 
 fun elvisOperator(){
 
@@ -7,4 +8,30 @@ fun elvisOperator(){
      * */
     val myVar:Int? = null
     println(myVar ?: "This is the default value")
+}
+
+fun assertionOperator(){
+
+    /**
+     This will throw KotlinNullPointerException
+     */
+    try {
+        val name: String? = null
+        val result = name!!
+        println(result)
+
+    }
+    catch(e:Exception){
+        println("Caught nullPointerException ${e.message}")
+    }
+
+    /**
+     This will not throe exception ,
+     if the value is null it will throw
+     */
+    val strName: String? = "Arvind"
+    val resultant = strName!!
+    println(resultant)
+
+
 }
