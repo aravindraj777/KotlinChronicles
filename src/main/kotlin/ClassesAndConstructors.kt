c6ae3e9f406af03edb46d43c62e0384a8616b647
Aravind <aravindmunna240@gmail.com>
2024-11-29 12:18:12 +0530

    Classes , primary and secondary constrcutors and init blocks

---
+++ src/main/kotlin/ClassesAndConstructors.kt
@@ -0,0 +1,11 @@
+class ClassesAndConstructors(val myVal: String){
+    constructor(myInt: Int): this("From secondary"){
+        println("MyInt $myInt")
+    }
+
+    init {
+        println("This is from primary constructor $myVal")
+    }
+
+
+}
